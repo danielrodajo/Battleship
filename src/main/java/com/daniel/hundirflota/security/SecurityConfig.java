ba520c83ef1d31ad8b9be9d8c875f5e0e09cb43d
danie <daniel.rodajo98@gmail.com>
2022-02-07 13:49:56 +0100

    .

--- src/main/java/com/daniel/hundirflota/security/SecurityConfig.java
+++ src/main/java/com/daniel/hundirflota/security/SecurityConfig.java
@@ -14,8 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.daniel.hundirflota.repository.AppUserRepository;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
